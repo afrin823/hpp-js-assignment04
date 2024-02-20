391db476c8887b6ac2d3ea8bab7bcc0439fef9a5
afrin823 <afrin12490@gmail.com>
2024-02-20 14:08:43 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+/// 01
+
+let myFavroutColor = 'black';
+let myDressColor = 'white';
+
+if(myFavroutColor == myDressColor){
+    console.log('It is my favroute color')
+}else{
+    console.log(`Dont't matching my dresss`)
+}
+
+/// 02
+
+let myLaptopPrice = 50000;
+let myBudget = 40000;
+
+if(myBudget <= myLaptopPrice){
+    console.log('Tahole ami laptop kibo');
+}else if(myBudget >= myLaptopPrice){
+    console.log('Tahole ami laptop kinte parbo na')
+}else{
+    console.log('Amr budger er sathe na mille ami laptop kinte parbo na')
+}
+
+// 03
+let myFoodPrice = 100;
+let myFoodBudget = 80;
+
+if(myFoodBudget <= myFoodPrice){
+    if(myFoodBudget == 150){
+        console.log('Tahole ami food khabo');
+    }else{
+        console.log('Noyto ami foid kinbo na');
+    }
+    
+}else if(myFoodBudget === myFoodPrice){
+    console.log('Tahole o ami food kinbo')
+}else if(myFoodBudget >= myFoodPrice){
+    console.log('tahole ami food kinte parbo na')
+}else{
+    console.log('Tahole ami kichui kinbo na')
+}
+
